89a39cc9f5185151c94669dd4ca61399a19d91fe
Jiyun Yun <jeeyunyun8@gmail.com>
2023-10-31 10:42:56 +0900

    Update annotation

--- BOJ/BOJ_1748.java
+++ BOJ/BOJ_1748.java
@@ -1,7 +1,8 @@
-import java.util.Scanner;
-
 // https://www.acmicpc.net/problem/1748
 // Silver 3 수 이어 쓰기 1
+
+import java.util.Scanner;
+
 public class BOJ_1748 {
 
 	public static void main(String[] args) {
