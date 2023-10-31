0c827314fe885a30be5692e1c6333ea242cd9dfb
Jiyun Yun <jeeyunyun8@gmail.com>
2023-10-31 10:44:14 +0900

    Update annotation

--- BOJ/BOJ_2858.java
+++ BOJ/BOJ_2858.java
@@ -1,7 +1,8 @@
-import java.util.Scanner;
-
 // https://www.acmicpc.net/problem/2858
 // Bronze 2 기숙사 바닥
+
+import java.util.Scanner;
+
 public class BOJ_2858 {
 
 	public static void main(String[] args) {
