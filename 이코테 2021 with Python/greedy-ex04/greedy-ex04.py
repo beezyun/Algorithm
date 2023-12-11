95b59d2a675cae98d08156e3efc5de9960e6b729
Jiyun Yun <jeeyunyun8@gmail.com>
2023-12-11 17:05:27 +0900

    solve: 이코테 2021 greedy exercise 04. 모험가 길드

---
+++ 이코테 2021 with Python/greedy-ex04/greedy-ex04.py
@@ -0,0 +1,24 @@
+# 내 풀이 답안
+num = int(input())
+fear = list(map(int, input().split()))
+
+group = 0
+member = 0
+
+# 공포도를 내림차순으로 정렬
+fear.sort()
+
+for i in fear:
+    member += 1
+    if member >= i:
+        group += 1
+        member = 0
+
+print(group)
+
+
+
+
+
+
+# 답안 예시
