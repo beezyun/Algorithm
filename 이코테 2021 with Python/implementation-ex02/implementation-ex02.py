6e0671a6ba1c632c888050bdddd8cce45c65c293
Jiyun Yun <jeeyunyun8@gmail.com>
2023-12-12 18:07:40 +0900

    solve: 이코테 2021 구현 exercise 02. 시각

--- 이코테 2021 with Python/implementation-ex02/implementation-ex02.py
+++ 이코테 2021 with Python/implementation-ex02/implementation-ex02.py
@@ -24,4 +24,18 @@ for i in range((n * 3600) + (59 * 60) + 59):
 
 print(result)
 
-# 답안 예시
+# # 답안 예시
+# # H 입력 받기
+# h = int(input())
+
+# count = 0
+
+# # i: 시, j: 분, k: 초
+# for i in range (h + 1):
+# 	for j in range(60):
+# 		for k in range(60):
+# 			# 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+# 			if '3' in str(i) + str(j) + str(k):
+# 				count += 1
+
+# print(count)
