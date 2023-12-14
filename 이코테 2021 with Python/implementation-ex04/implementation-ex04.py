b2f0850b196eadaad70516f06faa9500abad5769
Jiyun Yun <jeeyunyun8@gmail.com>
2023-12-14 12:36:09 +0900

    solve: 이코테 2021 구현 exercise 04. 문자열 재정렬

---
+++ 이코테 2021 with Python/implementation-ex04/implementation-ex04.py
@@ -0,0 +1,42 @@
+# 내 풀이 답안
+input = input()
+str = []
+num = 0
+
+for i in input:
+    if i >= 'A' and i <= 'Z':
+        # print(i)
+        str.append(i)
+    else:
+        num += int(i)
+
+str.sort()
+for s in str:
+    print(s, end = "")
+if num != 0:
+    print(num)
+
+# # 답안 예시
+# data = input()
+# result = []
+# value = 0
+
+# # 문자를 하나씩 확인하여
+# for x in data:
+#     # 알파벳인 경우 리스트에 삽입
+#     if x.isalpha():
+#         result.append(x)
+#     # 숫자는 따로 더하기
+#     else:
+#         value += int(x)
+
+# # 알파벳을 오름차순으로 정렬
+# result.sort()
+
+# # 숫자가 하나라도 존재하는 경우 가장 뒤에 삽입
+# if value != 0:
+#     result.append(str(value))
+
+# # 최종 결과 출력(리스트를 문자열로 변환하여 출력)
+# print(''.join(result))
+
